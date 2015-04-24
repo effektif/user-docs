dbf3e277695ab41dc2c39e18c1229d469c316970
Philipp Giese <giese.philipp+git@gmail.com>
2015-04-24 14:10:14 +0200

    #2 safety commit to not loose anything

--- docs/de/conf.py
+++ docs/de/conf.py
@@ -1,12 +1,14 @@
 import os
 import sys
 
+CWD = os.path.dirname(os.path.realpath(__file__))
 PROJECT_PATH = os.path.dirname(os.path.realpath('%s/..' % __file__))
 
 sys.path.append(PROJECT_PATH)
 
 from conf import *
 
-from i18n import copy_images
+from i18n import copy_images, copy_sources
 
 copy_images(lang='de', path=PROJECT_PATH)
+copy_sources(CWD, origin=PROJECT_PATH)
--- docs/i18n.py
+++ docs/i18n.py
@@ -1,6 +1,7 @@
 import os
+import re
 
-from shutil import copyfile, rmtree
+from shutil import copyfile, rmtree, copytree
 
 DEFAULT_LANG = 'en'
 PROJET_PATH = os.path.dirname(os.path.realpath(__file__))
@@ -23,3 +24,31 @@ def copy_images(lang=DEFAULT_LANG, path=PROJET_PATH):
                 copyfile(substitute_name, destination)
             else:
                 copyfile('%s/_static/_images/%s/%s' % (path, DEFAULT_LANG, file_name), destination)
+
+def copy_sources(dest, origin=PROJET_PATH):
+    BLACKLIST = [
+        '_build',
+        'Makefile',
+        '__index__.py',
+        'conf.py',
+        'conf.pyc',
+        dest.replace(PROJET_PATH, '')
+    ]
+
+    print "using %s as destination" % dest
+
+    for name in os.listdir(origin):
+        does_match = [re.search(item, name) for item in PATTERNS]
+
+        print does_match
+
+        if name in BLACKLIST:
+            print "ignoring %s" % name
+            continue
+
+        if os.path.isfile(name):
+            print "copy file %s to %s" % (name, "%s/%s" % (dest, name))
+            # copyfile(name, '%s/%s' % (dest, name))
+        else:
+            print "copy folder %s to %s" % (name, dest)
+            # copytree(name, dest)
