93d360bc2584ebcef4e83a86b22c470e3d574bfa
Philipp Giese <giese.philipp+git@gmail.com>
2015-04-24 14:34:55 +0200

    #2 a bit of code cleanup after we finally beat readthedocs

--- docs/de/conf.py
+++ docs/de/conf.py
@@ -1,8 +1,6 @@
 import os
 import sys
 
-from shutil import rmtree
-
 CWD = os.path.dirname(os.path.realpath(__file__))
 PROJECT_PATH = os.path.dirname(os.path.realpath('%s/..' % __file__))
 
@@ -10,21 +8,6 @@ sys.path.append(PROJECT_PATH)
 
 from conf import *
 
-from i18n import copy_images, copy_sources
+from i18n import prepare_language
 
-copy_images(lang='de', path=PROJECT_PATH)
-
-for name in os.listdir(CWD):
-    BLACKLIST = ['_build', 'conf.py', '.gitignore']
-
-    if name in BLACKLIST:
-        continue
-
-    if os.path.isfile(name):
-        # print 'removing file %s' % name
-        os.remove(name)
-    else:
-        # print 'removing dir %s' % name
-        rmtree(name)
-
-copy_sources(CWD, origin=PROJECT_PATH)
+prepare_language(CWD, lang='de')
--- docs/i18n.py
+++ docs/i18n.py
@@ -50,3 +50,25 @@ def copy_sources(dest, origin=PROJET_PATH):
         else:
             # print "copy folder %s to %s" % (name, dest)
             copytree(source, target)
+
+def remove_stale_data(path):
+    for name in os.listdir(path):
+        BLACKLIST = ['_build', 'conf.py', '.gitignore']
+
+        if name in BLACKLIST:
+            continue
+
+        if os.path.isfile(name):
+            # print 'removing file %s' % name
+            os.remove(name)
+        else:
+            # print 'removing dir %s' % name
+            rmtree(name)
+
+def prepare_language(cwd, path=PROJET_PATH, lang=DEFAULT_LANG):
+    copy_images(lang=lang, path=path)
+
+    remove_stale_data(cwd)
+
+    copy_sources(cwd, origin=path)
+
