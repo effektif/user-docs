4f98c6ac95ffbc3f543e746f5ad85996d5bdec5c
Timotheus Kampik <timotheus.kampik@signavio.com>
2016-07-11 09:44:08 +0200

    start: try to fix broken search results for rtd

--- docs/de/conf.py
+++ docs/de/conf.py
@@ -11,3 +11,8 @@ from conf import *
 language = 'de'
 
 prepare(CWD, PROJECT_PATH, language)
+
+import subprocess
+build_txt = subprocess.Popen(['sphinx-build', '-a', '-D', 'language="de"', '-b', 'text', '..', '../_build_txt'])
+build_txt.wait()
+subprocess.Popen(['mv', '../_build_txt/text', '/_build/html/_sources'])
