ad1c8971d40390d3b62b0d90d01f8b40acb0fc1a
Timotheus Kampik <timotheus.kampik@signavio.com>
2016-07-12 20:05:48 +0200

    remove old code that causes the rtd doc build to break #122

--- docs/conf.py
+++ docs/conf.py
@@ -45,17 +45,6 @@ def prepare(cwd, path, lang):
 
   touch('%s/_build/latex/EffektifUserGuide.tex' % cwd)
 
-  def build_search_snippets(app, docname):
-      shutil.rmtree('_build/html/_sources')
-      build_txt = subprocess.Popen(['sphinx-build', '-a', '-b', 'text', '..', '../_build_txt'])
-      build_txt.wait()
-      clean_txts(PROJECT_PATH)
-      shutil.move('../_build_txt/' + language, '_build/html')
-      shutil.move('_build/html/' + language, '_build/html/_sources')
-
-  def setup(app):
-      app.connect('build-finished', build_search_snippets)
-
 
 AUTHORS = [" ".join(pair) for pair in sorted([
   ('T.', 'Baeyens'),
