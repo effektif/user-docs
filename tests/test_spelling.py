60afb685281fa8f435f2587c6ccc880f79ebcd05
Timotheus Kampik <timotheus.kampik@signavio.com>
2016-12-12 09:52:30 +0100

    enable German spell check test #176

--- tests/test_spelling.py
+++ tests/test_spelling.py
@@ -4,12 +4,28 @@ from sphinx_testing import with_app
 
 @with_app(buildername='spelling', srcdir='../docs/en',
     copy_srcdir_to_tmpdir=False, outdir='root/_build')
-def setup(app, status, warning):
+def test_spelling_en(app, status, warning):
     app.build()
-    global lines
     with open(app.outdir + '/output.txt') as f:
         lines = f.readlines()
         lines = [line.replace('\n', '').replace('\r', '') for line in lines]
-
-def test_spelling():
     assert not lines, 'The spell checker found the following errors: %s' % lines
+
+@with_app(buildername='spelling', srcdir='../docs/de',
+    copy_srcdir_to_tmpdir=False, outdir='root/_build')
+def test_spelling_de(app, status, warning):
+    app.build()
+    exceptions = ['integration/connectors']
+    with open(app.outdir + '/output.txt') as f:
+        lines = f.readlines()
+        parsedLines = []
+        for line in lines:
+            isException = False
+            for exception in exceptions:
+                if exception in line:
+                    isException = True
+            if not isException:
+                parsedLines.append(line)
+        f = open(app.outdir + '/output.txt', 'w');
+        f.write(''.join(str(line) for line in parsedLines))
+    assert not parsedLines, 'The spell checker found the following errors: %s' % parsedLines
